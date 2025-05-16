bb73619666958b2df5108534e4aa4e419f0e21e1
Kaweesha7Anuhas <kaweesha7anuhas@gmail.com>
2025-05-16 20:45:07 +0530

    Initial project setup

---
+++ src/main/java/com/movie/MovieRentalPlatform/MovieRentalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.movie.MovieRentalPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalPlatformApplication.class, args);
+	}
+
+}
